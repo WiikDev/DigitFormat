fac36d5150153bc38c519d4afe87fb91b1c65adf
Steven <92732391+Wikeos@users.noreply.github.com>
2021-12-06 23:52:22 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+import datetime
+
+token = ""
+signature = ""
+
+def postRequest(token, startTime, endTime, slot):
+
+    # Date du jour
+    date = str(datetime.datetime.now())[0:10]
+
+
+    url = "https://app.digiforma.com/api/v1/graphql"
+    headers = {'Host': 'app.digiforma.com', 
+            'Connection': 'keep-alive', 
+            'accept': '*/*',
+            'content-type': 'application/json',
+            'authorization': 'Bearer ' + token,
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'Origin': 'https://cefim.digiforma.net',
+            'Sec-Fetch-Site': 'cross-site',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Dest': 'empty',
+            'Referer': 'https://cefim.digiforma.net/',
+            'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Accept-Encoding': 'gzip, deflate',
+            'Content-Length': '22420'}
+    data = {"operationName" : "sign_one_attendance",
+    "query" : "mutation sign_one_attendance($id: ID!, $date: TrainingSessionDateInput, $signature: String!, $on_behalf: ID) {\n  sign_one_attendance(id: $id, date: $date, signature: $signature, on_behalf: $on_behalf)\n}\n",
+    "variables" : {
+    "date" : {
+            "date" : date,
+            "end_time" : endTime,
+            "slot" : slot,
+            "start_time" : startTime
+            },
+    "id" : "588754",
+    "on_behalf" : None,
+    "signature" : signature}}
+    results = requests.post(url=url, data=data, headers=headers)
+    print(str(results))
+
+# Date du jour
+heure = int(str(datetime.datetime.now())[11:13])
+if(heure < 14):
+    startTime = "09:00:00"
+    endTime = "12:30:00"
+    slot = "morning"
+else:
+    startTime = "14:00:00"
+    endTime = "17:30:00"
+    slot = "afternoon"
+postRequest(token, startTime, endTime, slot)
